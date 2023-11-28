0303b865de03b769b1bb726374be860a9dc93fc6
SubhScripter <142106447+SubhScripter@users.noreply.github.com>
2023-11-28 18:55:20 +0000

    Video input

--- app.py
+++ app.py
@@ -1,59 +1,88 @@
-from ultralytics import YOLO
-from flask import request, Response, Flask
-from waitress import serve
-from PIL import Image
-import json
+import cv2
+import time
+import numpy as np
+from flask import Flask, render_template, Response
+from ultralytics import YOLO  # Make sure to import YOLO from ultralytics
 
 app = Flask(__name__)
 
-@app.route("/")
-def root():
-    """
-    Site main page handler function.
-    :return: Content of index.html file
-    """
-    with open("index.html") as file:
-        return file.read()
+# Load YOLOv8 model (make sure you have the YOLOv8 model files)
+yolo_model = YOLO("best.pt")
 
+# Video capture from webcam
+camera = cv2.VideoCapture(0)  # 0 corresponds to the default webcam device
 
-@app.route("/detect", methods=["POST"])
-def detect():
-    """
-        Handler of /detect POST endpoint
-        Receives uploaded file with a name "image_file", 
-        passes it through YOLOv8 object detection 
-        network and returns an array of bounding boxes.
-        :return: a JSON array of objects bounding 
-        boxes in format 
-        [[x1,y1,x2,y2,object_type,probability],..]
-    """
-    buf = request.files["image_file"]
-    boxes = detect_objects_on_image(Image.open(buf.stream))
-    return Response(
-      json.dumps(boxes),  
-      mimetype='application/json'
-    )
+# Set the desired frames per second for 30 frames per second
+desired_fps = 30
+frame_delay = 1 / desired_fps
 
+def generate_frames():
+    while True:
+        start_time = time.time()
 
-def detect_objects_on_image(img):
-    """
-    Function receives an image,
-    passes it through YOLOv8 neural network
-    and returns an array of detected objects
-    and their bounding boxes
-    :param buf: Input image file stream
-    :return: Array of bounding boxes in format 
-    [[x1,y1,x2,y2,object_type,probability],..]
-    """
-    model = YOLO("best.pt")
-    results = model.predict(img)
-    result = results[0]
-    output = []
-    for box in result.boxes:
-        x1, y1, x2, y2 = [round(x) for x in box.xyxy[0].tolist()]
-        class_id = box.cls[0].item()
-        prob = round(box.conf[0].item(), 2)
-        output.append([x1, y1, x2, y2, result.names[class_id], prob])
-    return output
+        success, frame = camera.read()  # Read the frame from the webcam
 
-serve(app, host='0.0.0.0', port=8080)
+        if not success:
+            break
+        else:
+            # Perform object detection with YOLOv8
+            objects = detect_objects(frame)
+            
+            # Perform voice output based on detected objects
+            voice_output = generate_voice_output(objects)
+
+            # Overlay the voice output on the frame
+            cv2.putText(frame, f"Voice Output: {voice_output}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # Encode the frame in JPEG format
+            _, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+
+            # Yield the frame in the response
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+        elapsed_time = time.time() - start_time
+        sleep_time = max(0, frame_delay - elapsed_time)
+        time.sleep(sleep_time)
+
+def detect_objects(frame):
+    # Preprocess the frame for YOLOv8
+    yolo_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    yolo_frame = cv2.resize(yolo_frame, (640, 480))  # Adjust the size as needed
+    
+    # Perform YOLOv8 object detection
+    results = yolo_model.predict(yolo_frame)
+    
+    # Post-process the detections
+    objects = []
+    for box in results.xyxy[0]:
+        x1, y1, x2, y2 = [round(coord.item()) for coord in box[:4]]
+        class_id = int(box[5].item())
+        prob = round(box[4].item(), 2)
+        class_name = results.names[class_id]
+        objects.append({'x1': x1, 'y1': y1, 'x2': x2, 'y2': y2, 'class': class_name, 'probability': prob})
+
+    return objects
+
+def generate_voice_output(objects):
+    # Your logic to generate voice output based on detected objects
+    # For example, check if a traffic light is detected and generate appropriate voice commands
+    voice_output = "No relevant objects detected"
+    for obj in objects:
+        if obj['class'] == 'traffic_light':
+            if obj['probability'] > 0.5:
+                voice_output = "Traffic light detected. Please follow the signal."
+                break
+    return voice_output
+
+@app.route('/')
+def index():
+    return render_template('index.html')  # Render the HTML template
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(debug=True)
